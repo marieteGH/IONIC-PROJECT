13e2ef048cab63e48a4c107747f6f37589316e9e
marieteGH <mariorodvaz05@gmail.com>
2025-10-27 19:18:00 +0100

    mockup login

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
-import { NavController,IonicModule } from '@ionic/angular';
-
-
+import { NavController, IonicModule } from '@ionic/angular';
+import { FormsModule } from '@angular/forms'; // ⬅️ ¡IMPORTACIÓN CLAVE!
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.page.html',
   styleUrls: ['./login.page.scss'],
+  
+  standalone: true, 
+  // Añadimos FormsModule a la lista de imports
+  imports: [IonicModule, FormsModule] 
 })
 export class LoginPage {
   email: string = '';
